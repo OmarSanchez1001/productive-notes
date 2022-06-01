0e48926ba28f1720ae368a2e3e0e9a9799cd76dd
Omar Sanchez <osanchez10@alumnos.uaq.mx>
2022-06-01 00:52:34 -0500

    ADD: Firebase-config.ts

---
+++ src/environments/firebase-config.ts
@@ -0,0 +1,8 @@
+export const firebaseConfig = {
+    apiKey: "AIzaSyCHwIU23WhZJv0xTHsypjWxeTI6-E6yx-w",
+    authDomain: "productive-notes-89ebb.firebaseapp.com",
+    projectId: "productive-notes-89ebb",
+    storageBucket: "productive-notes-89ebb.appspot.com",
+    messagingSenderId: "247084110138",
+    appId: "1:247084110138:web:9bb4b59d7250afd834c003"
+  };
