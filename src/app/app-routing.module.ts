a1036cb352bbf57fe10b9e35207c91caa5506899
Adrian Gonzalez <agonzalez168@alumnos.uaq.mx>
2022-06-03 23:51:06 -0500

    Creacion de tab 4

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -9,7 +9,11 @@ const routes: Routes = [
   {
     path: '',
     loadChildren: () => import('./login-register/login-register.module').then( m => m.LoginRegisterPageModule)
+  },  {
+    path: 'tab4',
+    loadChildren: () => import('./tab4/tab4.module').then( m => m.Tab4PageModule)
   }
+
 
 ];
 @NgModule({
